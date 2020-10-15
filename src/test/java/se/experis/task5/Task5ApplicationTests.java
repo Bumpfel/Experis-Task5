4df95c9ba540f070b92a85ffbc2f279b6912ac1b
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-10-15 11:55:01 +0200

    Initial commit

---
+++ src/test/java/se/experis/task5/Task5ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.task5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
